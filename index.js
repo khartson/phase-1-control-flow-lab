a51dd83fe553f8eeaba840b7efb8c7058b59c65a
Kyle <khartson2017@gmail.com>
2021-10-14 14:00:38 -0400

    commit tree 064c7cb9e954c27db239fae9befcfe466ab44680

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return("This one is on me!"); 
+  } else if (feet > 2000 && feet <= 2500) {
+    return("I will gladly take your thirty bucks."); 
+  } else if (feet > 2500) {
+    return("No can do.")
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let message; 
+  city === "NYC" ? (message = "Ok, sounds good.") : (message = "No go.");
+  return message; 
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generosity){
   // Write your code here!
+  switch(generosity) {
+    case "generous":
+      return("Thank you so much.");
+      break;
+    case "not as generous":
+      return("Thank you.");
+      break;
+    default:
+      return("Bye."); 
+  }
 }
